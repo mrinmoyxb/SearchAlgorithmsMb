0d9d5d88fce83aae9c084d0426cedf05270168e7
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-28 21:06:53 +0530

    puthon library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "searchAlgorithmsMb",
+    version= '0.4',
+    packages = find_packages(),
+    author = "Mrinmoy Borah",
+    long_description = "A simple python library that includes two search algorithms: Linear Search and Binary Search"
+)
+
+Homepage = "https://github.com/mrinmoyxb/SearchAlgorithmsMb"
